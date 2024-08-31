22e26b3bbc3a1eeae122b34492c5a8fa66cefb2f
Stephen Hellicar <shellicar@gmail.com>
2024-08-31 19:32:31 +1000

    Improve error classes with setPrototypeOf and name.

--- src/errors.ts
+++ src/errors.ts
@@ -1,19 +1,25 @@
 import type { ServiceIdentifier } from './types';
 
 export class UnregisteredServiceError<T extends object> extends Error {
+  name = 'UnregisteredServiceError';
   constructor(identifier: ServiceIdentifier<T>) {
     super(`Resolving service that has not been registered: ${identifier}`);
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
 export class MultipleRegistrationError<T extends object> extends Error {
+  name = 'MultipleRegistrationError';
   constructor(identifier: ServiceIdentifier<T>) {
     super(`Multiple services have been registered: ${identifier}`);
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
 export class SelfDependencyError extends Error {
+  name = 'SelfDependencyError';
   constructor() {
     super('Service depending on itself');
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
